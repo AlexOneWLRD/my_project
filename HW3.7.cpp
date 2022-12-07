f18e4583ea2d01f005d2df6cd6e785b224d1ca09
Aleksandr Ovsyannikov <116282471+AlexOneWLRD@users.noreply.github.com>
2022-12-07 13:42:37 +0300

    HW3.7 (ispr)

--- HW3.7.cpp
+++ HW3.7.cpp
@@ -4,11 +4,10 @@ int main()
 {
     int i=1;
     int sum=0;
-    for(;i<100;i=i+1)
+    for(;i<101;i=i+1)
     {
         sum=sum+i;
         printf("%i+",i);
     }
-    sum=sum+i;
     printf("%i=%i",i,sum);
 }
