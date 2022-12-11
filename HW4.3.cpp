bfbf37e9e54c4215b383670abecee10dc93039f8
Aleksandr Ovsyannikov <116282471+AlexOneWLRD@users.noreply.github.com>
2022-12-11 21:16:27 +0300

    HW4.3

---
+++ HW4.3.cpp
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+int main()
+{
+    int mas[100];
+    int n;
+    printf("kol el ");
+    scanf("%i",&n);
+    for(int i=0;i<n;i++)
+    {
+       scanf("%i",&mas[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+       printf("%4i",mas[i]);
+    }
+    printf("\n");
+
+bool fl=true;
+int  ch=0;
+while(fl)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        fl=false;
+        if (mas[i]>mas[i+1])
+        {
+            int z=mas[i];
+            mas[i]=mas[i+1];
+            mas[i+1]=z;
+            fl=true;
+        }
+    } 
+    ch++; 
+}
+     for(int i=0;i<n;i++)
+    {
+       printf("%4i",mas[i]);
+    }
+    printf("\n %i",ch);
+}
