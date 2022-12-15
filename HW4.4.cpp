842b30616c659a50a2b7382798d9c657a803ed54
Aleksandr Ovsyannikov <116282471+AlexOneWLRD@users.noreply.github.com>
2022-12-15 13:16:48 +0300

    HW4.4

---
+++ HW4.4.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main()
+{
+    int mas[100];
+    int n;
+
+    printf("kol el ");
+    scanf("%i",&n);
+    for (int i=0;i<n;i++)
+    {
+        scanf("%i",&mas[i]);
+    }
+int min,max;
+min=max=mas[0];
+
+int sum=0;
+
+for(int i=0;i<n;i++)
+{
+    if (mas[i]>max)
+    {
+        max=mas[i];
+    }
+    if (mas[i]<min)
+    {
+        min=mas[i];
+    }
+    sum+=mas[i];
+}
+
+float sr=(float)sum/(float)n;
+
+printf("sum=%i sr=%f min=%i max=%i",sum,sr,min,max);
+
+}
